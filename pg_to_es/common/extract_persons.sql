cf4fe25b1744fa4fae9d2d6c3e748129b535fa1f
BityutskiyNA <BityutskiyNA@gmail.com>
2022-12-12 17:40:02 +0300

    etl_done

--- pg_to_es/common/extract_persons.sql
+++ pg_to_es/common/extract_persons.sql
@@ -7,14 +7,14 @@ with persons as (
     from {from_schema}.person
 )
 select
-    json_build_object('id', g.id,
-                      'full_name', coalesce(g.name, ''),
-        )
-    ) as "xyz"
+    json_build_object('id', p.id,
+                      'name', coalesce(p.full_name, '')
+
+        ) as "xyz"
 from person p
 where
     (p.modified > '{date_from}' or p.created > '{date_from}')
   and
-    (pmodified <= '{date_to}' or p.created <= '{date_to}')
+    (p.modified <= '{date_to}' or p.created <= '{date_to}')
 order by p.id
 offset {offset};
